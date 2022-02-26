b76b27aa228348f3e96e408608dce60f5ee4a011
bbwieland <benbowenwieland@gmail.com>
2022-02-26 12:57:51 -0500

    file commit

--- NBAEloRatingsApp/app.R
+++ NBAEloRatingsApp/app.R
@@ -1,25 +1,22 @@
 library(shiny)
-library(NBAEloRatings)
 
-gt_tbl_picks <- data$DailyPicks$picktable
-gt_tbl_standings <- data$DivisionStandings
-gt_tbl_overall <- data$OverallStandings
+
+overall = read.csv("NBAEloRatingsApp/data/overall.csv")
+standings = read.csv("NBAEloRatingsApp/data/standings.csv")
+picks = read.csv("NBAEloRatingsApp/data/daily.csv")
 
 ui <- fluidPage(
-    
 
-    
-    
     titlePanel("Up-to-Date NBA Elo Standings"),
     fluidRow(
-        column(5,align="center",gt_output("standings")),
-        column(5,aligh="center",gt_output("overall"))
+        column(5,align="center",renderTable("standings")),
+        column(5,aligh="center",renderTable("overall"))
         ),
     
     titlePanel("Daily NBA Predictions"),
     
     fluidRow(
-        column(5,align="center",gt_output("picks"),offset = 1)
+        column(5,align="center",renderTable("picks"),offset = 1)
     )
 )
 
@@ -27,29 +24,11 @@ server <- function(input,
                    output,
                    session) {
     
-    output$standings <-
-        render_gt(
-            expr = gt_tbl_standings,
-            height = px(1300),
-            width = px(500),
-            align = "left"
-        )
+    output$standings <- tableOutput(standings)
     
-    output$overall <-
-        render_gt(
-            expr = gt_tbl_overall,
-            height = px(1300),
-            width = px(800),
-            align = "left"
-        )
+    output$overall <- tableOutput(overall)
     
-    output$picks <-
-        render_gt(
-            expr = gt_tbl_picks,
-            height = px(400),
-            width = px(1200),
-            align = "left"
-        )
+    output$picks <- tableOutput(picks)
     
     
 }
--- NBAEloRatingsApp/data/scraping.R
+++ NBAEloRatingsApp/data/scraping.R
@@ -3,3 +3,10 @@ library(NBAEloRatings)
 year = 2022
 data = overall.function.wrapper(year)
 
+overall = data$OverallStandings$OverallData
+standings = data$DivisionStandings$DivisionData
+daily = data$DailyPicks$fullpredictions %>% select(-dateGame,-idGame)
+
+write.csv(overall,"NBAEloRatingsApp/data/overall.csv")
+write.csv(standings,"NBAEloRatingsApp/data/standings.csv")
+write.csv(daily,"NBAEloRatingsApp/data/daily.csv")
